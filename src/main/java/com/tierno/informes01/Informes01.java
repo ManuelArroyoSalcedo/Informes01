abaf65a0aeeca32392271644e526dd42f439d9ea
Manuel Arroyo <manuel.arroyo@educa.madrid.org>
2025-05-21 14:25:45 +0200

    Primer commit - Proyecto Informes01

---
+++ src/main/java/com/tierno/informes01/Informes01.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tierno.informes01;
+
+/**
+ *
+ * @author Manuel
+ */
+public class Informes01 {
+
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setVisible(true);
+    }
+}
---
+++ src/main/java/com/tierno/informes01/MiJasper.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tierno.informes01;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Manuel
+ */
+public class MiJasper {
+
+    public MiJasper(Connection conexion,String archivo_con_ruta) {
+        
+    }
+    
+}
